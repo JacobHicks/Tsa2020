614443c509f3d264cf7724b37db73933f34238ae
Ethan Wainer <ethanwainer@berkeley.edu>
2019-10-19 20:24:51 +0000

    CreatePartyScreen.js edited online with Bitbucket

--- src/screens/CreatePartyScreen.js
+++ src/screens/CreatePartyScreen.js
@@ -1,56 +1,130 @@
-import React from "react";
-import DoubleTap from "../components/MultiTap";
-import PartyCard from "../components/PartyCard";
-import FeaturedPartyCard from "../components/FeaturedPartyCard";
-import SheetContent from "../components/SheetContent";
-import Footer from "../components/UIFooter";
-import { Text, View, StyleSheet, StatusBar } from "react-native";
-
-// import '@react-native-firebase/functions';
-// import '@react-native-firebase/auth';
-
-export default class CreatePartyScreen extends React.Component {
-	static navigationOptions = {
-		title: "Home"
-	};
-
-
-	constructor(props) {
-		super(props);
-		this.state = {
-
-			dbg: "tst",
-		};
-	}
+import React, { useState } from "react";
+import { Text, View, StyleSheet, SafeAreaView } from "react-native";
+import {
+	Container,
+	Header,
+	Content,
+	Form,
+	Item,
+	Input,
+	Left,
+	Right,
+	Button,
+	Icon,
+	Body,
+	Grid,
+	Col,
+	DatePicker
+} from "native-base";
 
 
+const CreatePartyScreen = function(props) {
+	// todo change the title of the party when they enter text
+	const [partyTitle, setPartyTitle] = useState("Untitled Party");
+	// setPartyTitle(() => {
+	//
+	// });
+	return (
+		<Container style={ styles.body }>
+			<Header style={ styles.header }>
+				<Left>
+					<Button transparent>
+						<Text style={ styles.headerButtonText }>Cancel</Text>
+					</Button>
+				</Left>
+				<Body>
+					<Text style={ styles.headerText }>{ partyTitle }</Text>
+				</Body>
+				<Right>
+					<Button transparent>
+						<Text style={ styles.headerButtonText }>Next</Text>
+					</Button>
+				</Right>
+			</Header>
+			<Content>
+				<Form style={ styles.form }>
+					<Item regular style={ styles.formItem }>
+						<Input placeholder="what's the plan?" style={ styles.titleInput }
+						       onChangeText={ setPartyTitle } />
+					</Item>
+					<Grid>
+						<Col>
+							<Item regular style={ styles.formItem }>
+								<Input placeholder="Where" style={ styles.formInput } />
+							</Item>
+						</Col>
+						<Col>
+							<Item regular style={ styles.formItem }>
+								<DatePicker
+									defaultDate={ new Date(2018, 4, 4) }
+									minimumDate={ new Date(2018, 1, 1) }
+									maximumDate={ new Date(2018, 12, 31) }
+									locale={ "en" }
+									timeZoneOffsetInMinutes={ undefined }
+									modalTransparent={ false }
+									animationType={ "slide" }
+									androidMode={ "default" }
+									placeHolderText="When"
+									textStyle={ { color: "#FFF" } }
+									placeHolderTextStyle={ { color: "#FFF" } }
+									onDateChange={ this.setDate }
+									disabled={ false }
+									style={ styles.datePicker }
+								/>
+							</Item>
+						</Col>
+					</Grid>
+				</Form>
+			</Content>
+		</Container>
+	);
+};
 
-
-	render() {
-		const { navigate } = this.props;
-		return (
-			<View style={ styles.body }>
-
-			</View>
-		);
-	}
-}
 
 const styles = StyleSheet.create({
 	body: {
-		backgroundColor: "#000"
+		backgroundColor: "#000",
+		height: "100%",
+		width: "100%"
 	},
 	header: {
-		paddingLeft: 10,
-		paddingTop: 60, // todo dynamically get statusbar height
 		borderBottomColor: "#555",
-		borderBottomWidth: .5
+		borderBottomWidth: .5,
+		backgroundColor: "#000"
 	},
 	headerText: {
-		color: "#FFFFFF"
+		color: "#FFF",
+		fontWeight: "700",
+		fontSize: 20
 	},
-	container: {
-		// padding: 7,
-		maxHeight: 750 // todo get device height - height of bottom navigation
+	headerButtonText: {
+		color: "#FFFFFF",
+		fontWeight: "600",
+		marginHorizontal: 15,
+		fontSize: 15
 	},
+	form: {
+		marginTop: 30
+	},
+	formItem: {
+		borderColor: "#000",
+		marginLeft: 15
+	},
+	formInput: {
+		color: "#fff"
+	},
+	titleInput: {
+		height: 80,
+		fontSize: 35,
+		color: "#FFF",
+		fontWeight: "700"
+	},
+	datePicker: {
+		color: "#fff"
+	},
+	testing: {
+		color: "#FFF"
+	}
 });
+
+export default CreatePartyScreen;
