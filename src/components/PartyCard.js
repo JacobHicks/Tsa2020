29e6e62d1d01efac9f35d52d9695d4787a21303f
Ethan Wainer <ethanwainer@berkeley.edu>
2019-10-24 09:06:41 +0000

    PartyCard.js edited online with Bitbucket

--- src/components/PartyCard.js
+++ src/components/PartyCard.js
@@ -1,35 +1,71 @@
 import React from "react";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Share } from "react-native";
 import {
 	Card,
 	CardItem,
 	Text,
 	Left,
-	Right
+	Right,
+	Button,
+	Grid,
+	Col
 } from "native-base";
 
+const userIsGoing = false; // todo get from props
+// todo fill in share info
+// todo implement Dynamic Link from firebase
+// todo implement join button
 export default class PartyCard extends React.Component {
+	onShare = async () => {
+		try {
+			const result = await Share.share({
+				message:
+					"Share [name] party with friends",
+				url: "https://google.com"
+			});
+			if (result.action === Share.sharedAction) {
+				if (result.activityType) {
+					// shared with activity type of result.activityType
+				} else {
+					// shared
+				}
+			} else if (result.action === Share.dismissedAction) {
+				// dismissed
+			}
+		} catch (error) {
+			alert(error.message);
+		}
+	};
+
+	formatDate(time) {
+		const formattedTime = new Date(time);
+		const months = ["JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "NOV", "DEC"];
+		return `${ months[formattedTime.getMonth() + 1] } ${ formattedTime.getDate() } @ ${ formattedTime.getHours() }:${ formattedTime.getMinutes() }`
+	}
+
 	render() {
+		// this.setState({dbg: this.props.userIsGoing})
 		return (
-			<Card transparent style={ styles.partyCard }>
-				<CardItem cardBody>
-					<View style={ { height: 200, width: null, flex: 1, backgroundColor: "#4834d4" } }>
-						<CalendarIcon style={ styles.calendarIcon } />
-					</View>
-
-					<View style={ styles.partyText }>
-						<Left>
-							<Text style={ styles.partyTitle }>{ this.props.title }</Text>
-							<View>
-								<Text style={ styles.partyInfo }>{ this.props.time } | { this.props.host }</Text>
-							</View>
-
-						</Left>
-						<Right>
-							{/*<Text>{ this.props.attendees }</Text>*/ }
-						</Right>
-					</View>
+			<Card transparent style={ [styles.partyCard, userIsGoing ? styles.userIsGoing : styles.defaultStyle] }>
+				<CardItem cardBody style={ [styles.cardBody, userIsGoing ? styles.userIsGoing : styles.defaultStyle] }>
+					<Text style={ styles.title } ellipsizeMode='tail' numberOfLines={ 2 }>{ this.props.title }</Text>
+					<Text style={ styles.dateTime }>{ this.formatDate(this.props.time) }</Text>
 				</CardItem>
+				<Text style={ styles.shortLocation } ellipsizeMode='tail' numberOfLines={ 1 }>
+					{ this.props.shortLocation }
+				</Text>
+				<Grid style={ styles.buttonGrid }>
+					<Col>
+						<Button style={ styles.shareButton } onPress={ this.onShare }>
+							<Text style={ styles.shareButtonText }></Text>
+						</Button>
+					</Col>
+					<Col>
+						<Button style={ styles.joinButton } onPress={ this.props.joinParty }>
+							<Text style={ styles.joinButtonText }></Text>
+						</Button>
+					</Col>
+				</Grid>
 			</Card>
 		);
 	}
@@ -38,29 +74,74 @@ export default class PartyCard extends React.Component {
 const styles = StyleSheet.create({
 	partyCard: {
 		padding: 0,
-		marginBottom: 10,
-		minHeight: 120,
-		minWidth: 220
-		// borderRadius: 6.67
+		backgroundColor: "#000",
+		paddingBottom: 30,
+		maxWidth: 375
 	},
-	partyTitle: {
-		bottom: 25,
-		textAlign: "left",
-		paddingTop: "10%",
-		color: "#FFFFFF",
-		fontSize: 30,
-		fontWeight: "600",
+	cardBody: {
+		backgroundColor: "#000",
+		borderRadius: 0,
+		flexDirection: "row"
+	},
+	userIsGoing: {
+		backgroundColor: "#222"
+	},
+	defaultStyle: {},
+	dateTime: {
+		color: "#ee5253",
 		position: "absolute",
-		left: 12
-	},
-	partyText: {
-		position: "absolute",
-		bottom: 10,
-		color: "#FFF"
-	},
-	partyInfo: {
-		color: "#FFF",
+		top: 7,
 		left: 14,
+		fontSize: 14,
+		fontWeight: "700"
+	},
+	title: {
+		color: "#fff",
+		fontWeight: "700",
+		fontSize: 24,
+		left: 14,
+		top: 24
+	},
+	shortLocation: {
+		color: "#999",
+		fontWeight: "600",
+		marginTop: 37,
+		marginLeft: 14,
+		fontSize: 16,
+		maxWidth: 230
+	},
+	host: {
+		fontSize: 14,
+		color: "#999",
+		position: "absolute",
+		left: 14,
+		top: 22,
+		fontWeight: "600"
+	},
+	buttonGrid: {
+		marginTop: -25,
+		maxWidth: 75,
+		marginLeft: 270
 
+	},
+	joinButton: {
+		height: 30,
+		width: 75,
+		backgroundColor: "#ee5253"
+	},
+	joinButtonText: {
+		textAlign: "center",
+		fontWeight: "700",
+		fontSize: 12
+	},
+	shareButton: {
+		height: 30,
+		width: 30,
+		backgroundColor: "#ee5253"
+	},
+	shareButtonText: {
+		textAlign: "center",
+		fontWeight: "700",
+		fontSize: 12
 	}
 });
