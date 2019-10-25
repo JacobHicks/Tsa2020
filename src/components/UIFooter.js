90e4074a1859e11d253f38fbbaf97dab59d7be62
Ethan Wainer <ethan@wainermail.com>
2019-10-25 02:22:39 -0700

    styled footer

--- src/components/UIFooter.js
+++ src/components/UIFooter.js
@@ -1,50 +1,51 @@
-import React from 'react';
-import {Dimensions, StyleSheet, View} from 'react-native';
-import {Footer, FooterTab, Button, Text} from 'native-base';
-import Icon from 'react-native-vector-icons/MaterialIcons';
+import React from "react";
+import { Dimensions, StyleSheet, View } from "react-native";
+import { Footer, FooterTab, Button, Text } from "native-base";
+import Icon from "react-native-vector-icons/MaterialIcons";
 
-const UIFooter = function (props) {
-    const navigation = props.navigation;
-    return (
-        <Footer style={styles.footer}>
-            <FooterTab style={styles.footerTab}>
-                <Button active={props.createIsActive} style={styles.footerButton}
-                        onPress={() => navigation.navigate('CreateParty')}
-                >
-                    <Icon name='control-point' size={43} color='#FFF'/>
-                    <Text style={styles.footerButtonText}>Host</Text>
-                </Button>
-                <Button active={props.profileIsActive} style={styles.footerButton}
-                        onPress={() => navigation.navigate('Profile')}
-                >
-                    <Icon name='person' size={43} color='#FFF'/>
-                    <Text style={styles.footerButtonText}>Profile</Text>
-                </Button>
-            </FooterTab>
-        </Footer>
-    );
+const UIFooter = function(props) {
+	const navigation = props.navigation;
+	return (
+		<Footer style={ styles.footer }>
+			<FooterTab style={ styles.footerTab }>
+				<Button active={ props.createIsActive } style={ styles.footerButton }
+				        onPress={ () => navigation.navigate("CreateParty") }
+				>
+					<Icon name='control-point' size={ 20 } color='#FFF' />
+					{/*<Text style={styles.footerButtonText}>Host</Text>*/ }
+				</Button>
+				<Button active={ props.profileIsActive } style={ styles.footerButton }
+				        onPress={ () => navigation.navigate("Profile") }
+				>
+					<Icon name='person' size={ 20 } color='#FFF' />
+					{/*<Text style={styles.footerButtonText}>Profile</Text>*/ }
+				</Button>
+			</FooterTab>
+		</Footer>
+	);
 };
 
 const styles = StyleSheet.create({
-    footer: {
-        backgroundColor: '#000',
-        borderTopWidth: 1,
-        borderColor: '#FFF',
-        height: 88,
-    },
-    footerTab: {
-        backgroundColor: '#000',
-    },
-    footerButton: {
-        backgroundColor: '#000',
-        borderRadius: 0,
-    },
-    footerButtonText: {
-        color: '#FFF',
-        fontSize: 13,
-        textAlign: 'center',
-        fontWeight: '700',
-    },
+	footer: {
+		backgroundColor: "#555",
+		borderTopWidth: .5,
+		borderColor: "#FFF",
+		height: 50
+	},
+	footerTab: {
+		backgroundColor: "#000"
+	},
+	footerButton: {
+		backgroundColor: "#000",
+		borderRadius: 0,
+		marginBottom: 12
+	},
+	footerButtonText: {
+		color: "#FFF",
+		fontSize: 13,
+		textAlign: "center",
+		fontWeight: "700"
+	}
 
 
 });
