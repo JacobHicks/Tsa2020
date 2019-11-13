5a946584f0575acc7c3447dbaeef67b4466486d5
Ethan Wainer <ethan@wainermail.com>
2019-11-12 19:38:50 -0800

    featured party card design changes

---
+++ src/components/TouchableIcon.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+import { Button, Text } from "native-base";
+
+const TouchableIcon = function(props) {
+	return (
+		<Button style={ styles.button }>
+			<Text style={ styles.text }>{ props.children }</Text>
+		</Button>
+	);
+};
+
+const styles = StyleSheet.create({
+	button: {
+		backgroundColor: "transparent",
+		borderColor: "transparent"
+	},
+	text: {
+		color: "#333",
+		fontWeight: "700",
+		fontSize: 16
+	}
+
+});
+
+export default TouchableIcon;
